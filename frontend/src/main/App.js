5be933fb4337d33401441d6e131c1029ea0bf4a7
XandyGomes <x3.computadores@hotmail.com>
2022-06-09 20:09:40 -0300

    Aula 09.06.2022

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import Login from '../views/login';
 
-import 'bootswatch/dist/flatly/bootstrap.css';
+import Rotas from './rotas'
+import Navbar from '../components/navbar'
+import ProvedorAutenticacao from './provedorAutenticacao'
+
+import 'toastr/build/toastr.min'
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+import '../custom.css'
+import 'toastr/build/toastr.css'
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Login />
-      </div>
-    );
+
+  render(){
+    return(
+      <ProvedorAutenticacao>
+        <Navbar />
+        <div className="container">    
+            <Rotas />
+        </div>
+      </ProvedorAutenticacao>
+    )
   }
 }
-export default App;
+
+export default App
